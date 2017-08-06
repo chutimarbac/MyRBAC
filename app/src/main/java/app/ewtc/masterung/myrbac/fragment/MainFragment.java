58310b9cb5bce7c0016c1a3aaa6b0be8b57b797a
masterUNG <c.it.rbac@gmail.com>
2017-08-06 12:13:23 +0700

    before launch 6aug2560

--- app/src/main/java/app/ewtc/masterung/myrbac/fragment/MainFragment.java
+++ app/src/main/java/app/ewtc/masterung/myrbac/fragment/MainFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import app.ewtc.masterung.myrbac.R;
 
@@ -21,5 +22,32 @@ public class MainFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_fragment, container, false);
         return view;
+
+    }  //onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Register Controller
+        registerController();
+
     }
-}//Main Class นี่คือ คลาสหลัก
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SignUpFragment signUpFragment = new SignUpFragment();
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.mainContainer,signUpFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+}  //Main Class นี่คือ คลาสหลัก
